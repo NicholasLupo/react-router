6b3b34bbff582da03bf1821a85a0861f17b2e7e7
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-21 14:49:05 +0200

    add single card product

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom"
 export default function ProductPage() {
 
     const { id } = useParams()
-    const [product, setProduct] = useState([])
+    const [product, setProduct] = useState({})
 
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/${id}`)
@@ -12,15 +12,21 @@ export default function ProductPage() {
             .then(data => {
                 setProduct(data)
             })
-    }, [id])
+    }, [])
 
     return (
         <>
             <section>
                 <div className="container">
                     <div className="p-5 mb-4 bg-light rounded-3">
-                        <div className="container-fluid py-5 text-center">
-                            <h1>Product id: {id}</h1>
+                        <div className="container-fluid py-5">
+                            <img src={product.image} alt="" className="p-3" />
+                            <p><strong>Nome: </strong>{product.title}</p>
+                            <p><strong>Descrizione: </strong>{product.description}</p>
+                            <p><strong>Prezzo: </strong>{product.price}€</p>
+                            <button className="btn btn-success btn-lg">
+                                Acquista <i class="bi bi-cart"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
