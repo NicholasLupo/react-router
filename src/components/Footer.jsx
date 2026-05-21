d6510d72cafc47b21b7a0838d0f4a1b267313c46
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-21 14:05:26 +0200

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+
+
+    return (
+        <>
+            <div className="row bg-light p-4 mt-4 d-flex justify-content-around">
+                <div className="col-4">
+                    <ul className="list-unstyled">
+                        <li><h3>Info</h3></li>
+                        <li><p>About</p></li>
+                        <li><p>Contact</p></li>
+                    </ul>
+                </div>
+                <div className="col-4">
+                    <p>@2026 - Fakestore</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Presentation from "../pages/Presentation";
 import Products from "../pages/Products";
+import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
 
@@ -10,6 +11,7 @@ export default function DefaultLayout() {
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     )
 }
